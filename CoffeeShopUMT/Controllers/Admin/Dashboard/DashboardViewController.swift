6931637b5b2ec1ccbbebbb69d3b5a2e8c4cd68fa
bvnho <nho.2302700189@st.umt.edu.vn>
2026-03-27 09:38:09 +0700

    update

--- CoffeeShopUMT/Controllers/Admin/Dashboard/DashboardViewController.swift
+++ CoffeeShopUMT/Controllers/Admin/Dashboard/DashboardViewController.swift
@@ -13,16 +13,4 @@ final class DashboardViewController: UIViewController {
         super.viewDidLoad()
         // Giao diện đã được vẽ trên Storyboard nên ta không cần gọi setupUI() đổi màu nền ở đây nữa
     }
-    
-    
-    
-    
-    @IBAction func goToAnalyticsTapped(_ sender: UIButton) {
-        // Đã sửa lại tên ID thành "AnalyticsViewController" cho khớp với cài đặt của bạn
-        if let storyboard = self.storyboard,
-           let analyticsVC = storyboard.instantiateViewController(withIdentifier: "AnalyticsViewController") as? AnalyticsViewController {
-            
-            self.navigationController?.pushViewController(analyticsVC, animated: false)
-        }
-    }
 }
